ec1fc7fb19b77b467e3aa6d32d5a7e8de800cb42
Thanasis Kyprianos <than20022004@gmail.com>
2024-04-20 17:46:38 +0300

    Styled a bit

--- DjangoAPI/DjangoAPI/views.py
+++ DjangoAPI/DjangoAPI/views.py
@@ -1,12 +1,33 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+import csv
+from io import StringIO
+
+class Parser:
+	def __init__(self, csvString):
+		self.csvString = str(csvString)[2:-1].replace('\\r\\n', '\n')
+		# print(self.csvString)
+
+	def parse(self):
+		with StringIO(self.csvString, newline='') as inCSV:
+			csv_reader = csv.reader(inCSV, delimiter=';')
+			for row in csv_reader:
+				print(row)
+
+
 def main(request):
 	return HttpResponse('Django is working!')
 
 @csrf_exempt
 def test(request):
-	print(request.body)
+	# print(request.body)
+	# print(request.FILES['file'].name)
+	# print(request.FILES['file'].name)
+	# print(request.FILES['file'].read())
+
+	p = Parser(request.FILES['file'].read())
+	p.parse()
 
 	response = HttpResponse('Hello, World!')
 	response['Access-Control-Allow-Origin'] = 'http://localhost:4200'
